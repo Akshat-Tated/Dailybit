ea693cd5bf7cf4ac026323c887c407523e5aabc3
Akshat-Tated <akshattated18@gmail.com>
2025-05-12 02:40:04 +0530

    added Hashrouter

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    tailwindcss(),
-  ],
+  base: '/Dailybit/',  // 👈 This is the fix!
+  plugins: [react(), tailwindcss()],
 })
